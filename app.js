01623e0ec521a90e3eebb9080b6ea11dd68ccddf
codaboba <iamphanle@gmail.com>
2018-08-03 11:05:55 -0500

    added models folder

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const layout = require('./views/layout');
+const { Page } = require('./models');
 
 const app = express();
 
+Page.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
 app.use(morgan('dev'));
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send(layout(''));
+  res.send(layout(''));
 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`);
-  });
+  console.log(`App listening in port ${PORT}`);
+});
