43eddd5429202bdcb1f3d1f4c93a3c26c24e8f47
codaboba <iamphanle@gmail.com>
2018-08-03 14:15:57 -0500

    updated POST route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { Page } = require('../models');
 const addPage = require('../views/addPage');
+
+// router.use(express.json());
 
 router.get('/', async (req, res, next) => {
   try {
     res.redirect('../');
-  } catch (error) { console.error(error); }
+  } catch (error) {
+    console.error(error);
+  }
 });
 router.post('/', async (req, res, next) => {
-  res.send('got to POST /wiki');
+  // res.send('got to POST /wiki');
+  res.json(req.body);
 });
 router.get('/add', async (req, res, next) => {
   try {
     res.send(addPage());
-  } catch (error) { console.error(error); }
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 module.exports = router;
--- views/addPage.js
+++ views/addPage.js
@@ -1,25 +1,26 @@
-const html = require("html-template-tag");
-const layout = require("./layout");
+const html = require('html-template-tag');
+const layout = require('./layout');
 
-module.exports = () => layout(html`
+module.exports = () =>
+  layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
+
     <div class="form-group">
-      <label for="name" class="col-sm-2 control-label">Author name</div>
+      <label for="name" class="col-sm-2 control-label">Author name</label>
       <div class="col-sm-10">
         <input id="name" name="name" type="text" class="form-control" value="PLACEHOLDER FOR AUTHOR NAME FIELD"/>
       </div>
     </div>
 
     <div class="form-group">
-      <label for="email" class="col-sm-2 control-label">Author email</div>
+      <label for="email" class="col-sm-2 control-label">Author email</label>
       <div class="col-sm-10">
         <input id="email" name="email" type="text" class="form-control" value="PLACEHOLDER FOR AUTHOR EMAIL FIELD"/>
       </div>
     </div>
-    
+
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
@@ -28,14 +29,14 @@ module.exports = () => layout(html`
     </div>
 
     <div class="form-group">
-      <label for="content" class="col-sm-2 control-label">Article Content</div>
-      <div class="col-sm-50">
-        <input id="content" name="content" type="text" class="form-control" value="PLACEHOLDER FOR CONTENT FIELD"/>
+      <label for="content" class="col-sm-2 control-label">Article Content</label>
+      <div class="col">
+        <textarea id="content" name="content" class="form-control" placeholder="PLACEHOLDER FOR CONTENT FIELD" rows="10"></textarea>
       </div>
     </div>
-    
+
     <div class="form-group">
-      <label for="status" class="col-sm-2 control-label">Status</div>
+      <label for="status" class="col-sm-2 control-label">Status</label>
       <div class="col-sm-10">
         <input id="status" name="status" type="text" class="form-control" value="PLACEHOLDER FOR STATUS INPUT FIELD"/>
       </div>
@@ -44,6 +45,6 @@ module.exports = () => layout(html`
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
