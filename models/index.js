ff2996132e6be3f66f65a8ba4357a9ab19dd2298
codaboba <iamphanle@gmail.com>
2018-08-03 14:52:00 -0500

    setup up hook for slug

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack', {
-  logging: false
+  logging: false,
 });
+
+function generateSlug(title) {
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
 
 db.authenticate().then(() => {
   console.log('connected to the database');
@@ -14,39 +18,41 @@ const Page = db.define('page', {
   },
   slug: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   content: {
     type: Sequelize.TEXT,
-    allowNull: false
+    allowNull: false,
   },
   status: {
-    type: Sequelize.ENUM('open', 'closed')
+    type: Sequelize.ENUM('open', 'closed'),
   },
-});
+},
+
+  hooks: {
+    beforeValidate: (page, options) => {
+      page.slug = generateSlug(page.title);
+    }
+  }
+);
 
 const User = db.define('user', {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      isAlphanumeric: true
-    }
+      isAlphanumeric: true,
+    },
   },
   email: {
     type: Sequelize.STRING,
     allowNull: false,
     unique: true,
     validate: {
-      isEmail: true
-    }
+      isEmail: true,
+    },
   },
 });
-
-function generateSlug(titlename){
-  var reggae = new RegExp();
-  return String.prototype.replace(titlename);
-}
 
 module.exports = {
   db,
