cf54cf4c82589d5ad3d6c573675eb01a8baa07ac
Alex Morson <amorson@conncoll.edu>
2018-08-05 19:07:28 -0500

    all author related tasks complete

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const { User } = require('../models');
+const userPages = require('../views/userPages');
+const userList = require('../views/userList');
 const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.send(userList(users));
+    } catch (error) {
+        next(error);
+    }
+});
+
+// router.get('/:id', (req, res, next) => {
+
+// });
 
 module.exports = router;
